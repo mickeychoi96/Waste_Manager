60b6936288ef589eaf8677efbfe9e0f8a998872e
최유현 <yuhyeon_choi@choeyuhyeon-ui-MacBookAir.local>
2023-10-26 19:08:52 +0600

    Initial Commit

---
+++ Waste Manager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Waste Manager
+//
+//  Created by 최유현 on 10/26/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
